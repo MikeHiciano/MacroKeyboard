8e48176103f47e2537fa22a432416ccae0f4fba4
Miguel Hiciano <mike29hiciano@gmail.com>
2021-09-29 11:30:21 -0400

    adding 6 more buttons to the code

--- main.py
+++ main.py
@@ -6,25 +6,85 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-trm_btn = DigitalInOut(board.GP21)
-trm_btn.direction = Direction.INPUT
-trm_btn.pull = Pull.UP
+btn1 = DigitalInOut(board.GP2)
+btn1.direction = Direction.INPUT
+btn1.pull = Pull.UP
 
-ext_trm_btn = DigitalInOut(board.GP20)
-ext_trm_btn.direction = Direction.INPUT
-ext_trm_btn.pull = Pull.UP
+btn2 = DigitalInOut(board.GP3)
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.UP
+
+btn3 = DigitalInOut(board.GP4)
+btn3.direction = Direction.INPUT
+btn3.pull = Pull.UP
+
+btn4 = DigitalInOut(board.GP5)
+btn4.direction = Direction.INPUT
+btn4.pull = Pull.UP
+
+btn5 = DigitalInOut(board.GP6)
+btn5.direction = Direction.INPUT
+btn5.pull = Pull.UP
+
+btn6 = DigitalInOut(board.GP7)
+btn6.direction = Direction.INPUT
+btn6.pull = Pull.UP
+
+btn7 = DigitalInOut(board.GP8)
+btn7.direction = Direction.INPUT
+btn7.pull = Pull.UP
+
+btn8 = DigitalInOut(board.GP9)
+btn8.direction = Direction.INPUT
+btn8.pull = Pull.UP
 
 keyboard = Keyboard(usb_hid.devices)
 
 while True:
-    if not trm_btn.value:
+    if not btn1.value:
+        keyboard.press(Keycode.CONTROL,Keycode.C)
+        time.sleep(0.05)
+        keyboard.release_all()
+        time.sleep(0.5)
+
+    if not btn2.value:
+        keyboard.press(Keycode.CONTROL, Keycode.V)
+        time.sleep(0.05)
+        keyboard.release_all()
+        time.sleep(0.5)
+
+    if not btn3.value:
         keyboard.press(Keycode.CONTROL, Keycode.ALT, Keycode.T)
         time.sleep(0.05)
         keyboard.release_all()
-        time.sleep(0.2)
+        time.sleep(0.5)
 
-    if not ext_trm_btn.value:
-        keyboard.press(Keycode.E, Keycode.X, Keycode.I, Keycode.T ,Keycode.ENTER)
+    if not btn4.value:
+        keyboard.press(Keycode.E, Keycode.X, Keycode.I,Keycode.T,Keycode.ENTER)
         time.sleep(0.05)
         keyboard.release_all()
-        time.sleep(0.2)
+        time.sleep(0.5)
+
+    if not btn5.value:
+        keyboard.press(Keycode.CONTROL, Keycode.S)
+        time.sleep(0.05)
+        keyboard.release_all()
+        time.sleep(0.5)
+
+    if not btn6.value:
+        keyboard.press(Keycode.CONTROL, Keycode.ALT, Keycode.T)
+        time.sleep(0.05)
+        keyboard.release_all()
+        time.sleep(0.5)
+
+    if not btn7.value:
+        keyboard.press(Keycode.CONTROL, Keycode.ALT, Keycode.T)
+        time.sleep(0.05)
+        keyboard.release_all()
+        time.sleep(0.5)
+
+    if not btn8.value:
+        keyboard.press(Keycode.CONTROL, Keycode.ALT, Keycode.T)
+        time.sleep(0.05)
+        keyboard.release_all()
+        time.sleep(0.5)
